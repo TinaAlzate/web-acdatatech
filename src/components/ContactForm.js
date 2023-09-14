756d4b4ae97b1315df247a6b1681018643bc89cc
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-09-14 14:43:48 +0200

    dades fetching

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,15 +4,16 @@ import { useLocation } from 'react-router-dom'
 import { ExternalLink } from './Footer'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import '../styles/contact.css'
+import axios from 'axios'
 
 export const ContactForm = () => {
   const contactSchema = Yup.object().shape(
     {
       email: Yup.string().trim().email('Formato de email invalido').required('Email es un campo obligatorio'),
       service: Yup.string().trim().required('Servicios es un campo obligatorio'),
-      message: Yup.string().trim().required('Mensaje es un campo obligatorio').uppercase(),
-      checkbox_req: Yup.boolean().required('Debes aceptar nuestra política de privacidad para poder continuar'),
-      checkbox_opc: Yup.boolean()
+      message: Yup.string().trim().required('Mensaje es un campo obligatorio').min(10, 'Minímo 10 caracteres').max(500, 'Máximo 500 caracteres'),
+      conditions: Yup.boolean().required('Debes aceptar nuestra política de privacidad para poder continuar').oneOf([true], 'Se deben aceptar los términos y condiciones'),
+      bulkEmail: Yup.boolean()
     }
   )
 
@@ -27,12 +28,24 @@ export const ContactForm = () => {
     { value: 'business-optimization', label: 'Optimización de negocio' },
     { value: 'business-intelligence', label: 'Inteligencia de negocio' }
   ]
-
   const selectedOption = selectOptions.find(service => `/${service.value}` === pathname)
 
+  const URL_API = 'http://localhost:3003/admin/clients/create'
+
+  // const funhandleSubmit = async (values) => {
+  //   axios.post(URL_API, values)
+  //     .then(function (response) {
+  //       console.log(values)
+  //       const { token, user } = response
+  //       window.Cookies.set('token', token)
+  //       console.log(response)
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error)
+  //     })
+  // }
   const funhandleSubmit = (values) => {
-    console.log(values) // Aquí hacemos la petición
-    fetch()
+    console.log(values)
   }
 
   const initialCredentials = {
@@ -40,8 +53,8 @@ export const ContactForm = () => {
     email: '',
     message: '',
     service: selectedOption ? selectedOption.value : '',
-    checkbox_req: false,
-    checkbox_opc: false
+    conditions: '',
+    bulkEmail: ''
   }
 
   return (
@@ -115,27 +128,27 @@ export const ContactForm = () => {
               <ErrorMessage name="message" component='span'></ErrorMessage>
             )
           }
-          <label className="checkbox-label" htmlFor="checkbox_req">
+          <label className="checkbox-label" htmlFor="conditions">
             <Field
               className="input"
               type="checkbox"
-              id="checkbox_req"
-              name="checkbox_req"
+              id="conditions"
+              name="conditions"
             />
             <span>He leído y acepto <ExternalLink to="/privacy-policy" children='política de privacidad' /></span>
           </label>
           {
-            errors.checkbox && touched.checkbox &&
+            errors.conditions && touched.conditions &&
             (
-              <ErrorMessage name="checkbox_req" component='div'></ErrorMessage>
+              <ErrorMessage name="conditions" component='span'></ErrorMessage>
             )
           }
-          <label className="checkbox-label" htmlFor="checkbox_opc">
+          <label className="checkbox-label" htmlFor="bulkEmail">
             <Field
               className="input"
               type="checkbox"
-              id="checkbox_opc"
-              name="checkbox_opc"
+              id="bulkEmail"
+              name="bulkEmail"
             />
             <span>Nos gustaría que nos prestaras tu consentimiento para enviarte información comercial sobre los productos, servicios y/o novedades de AC data tech.</span>
           </label>
