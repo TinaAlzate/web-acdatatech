96ac8d7a477c8876d0f3e2eb606af2370804ef6a
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-09-19 21:52:16 +0200

    Implementación del modal de unsubscribe y sppiner.

--- unsubscribe/unsubcribeJS/unsubcribe.js
+++ unsubscribe/unsubcribeJS/unsubcribe.js
@@ -1,14 +1,23 @@
-document.getElementById('bajaACdatatech').addEventListener('click', () => {
+const conatinerMain = document.querySelector('.conatinerUnsubscribe')
+const modalRequest = document.querySelector('.modalHidden')
+const buttonModal = document.querySelector('.closeModal')
+const spinnerModal = document.querySelector('.sppiner')
+
+const unsubscribeCallback = (evento) => {
   const urlSearchParams = new URLSearchParams(window.location.search)
   const params = Object.fromEntries(urlSearchParams.entries())
   const { token } = params
-  console.log(token)
   fechingUnsubcribe(token)
-})
+  evento.target.setAttribute('disabled', '')
+}
+
+document.getElementById('bajaACdatatech').addEventListener('click', unsubscribeCallback)
 
 const API_AC = 'http://localhost:3003/admin/client/unsubcribe'
 
 const fechingUnsubcribe = (token) => {
+  conatinerMain.classList.add('opacity-50')
+  spinnerModal.classList.remove('hidden')
   fetch(API_AC, {
     method: 'DELETE',
     headers: {
@@ -16,9 +25,26 @@ const fechingUnsubcribe = (token) => {
     }
   })
     .then((response) => {
-      console.log('Response 👍🏽', response)
+      if (response.ok) {
+        activeModal('Response ok usuario eliminado!!')
+      }
     })
     .catch((error) => {
       console.log('Error ❌ ', error)
+      activeModal('No encontrado!!')
     })
 }
+
+const activeModal = (text) => {
+  conatinerMain.classList.add('opacity-50')
+  modalRequest.classList.remove('hidden')
+  const sectionModal = document.querySelectorAll('section')
+  console.log(sectionModal)
+  sectionModal[0].children[0].innerText = text
+  spinnerModal.classList.add('hidden')
+}
+
+buttonModal.addEventListener('click', () => {
+  conatinerMain.classList.remove('opacity-50')
+  modalRequest.classList.add('hidden')
+})
