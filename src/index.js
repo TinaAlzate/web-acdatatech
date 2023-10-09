d78674748d576e6f5b34a59e25ecda897141c1b5
Valentina Alzate <valzate618@gmail.com>
2023-10-09 14:13:14 +0200

    Update links

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
