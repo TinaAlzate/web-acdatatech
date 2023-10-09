d65b105cf5ce5ad01c0b8fb1641a604fef74530f
Valentina Alzate <valzate618@gmail.com>
2023-10-09 14:10:04 +0200

    Update links

--- src/App.js
+++ src/App.js
@@ -2,28 +2,30 @@
 import LayoutPublic from './layouts/LayoutPublic'
 import { useEffect } from 'react'
 import { SecurityDetails, MaintenanceDetails, CloudDetails, WebDetails, OptimizationDetails, IntelligenceDetails, Home, Contact, PrivacyPolicy, CookiesPolicy, NotFound } from './pages'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { ModalCookies } from './components'
 
 const App = () => {
   return (
     <>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<LayoutPublic />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/security" element={<SecurityDetails />} />
-          <Route path="/maintenance" element={<MaintenanceDetails />} />
-          <Route path="/cloud" element={<CloudDetails />} />
-          <Route path="/web-development" element={<WebDetails />} />
-          <Route path="/business-optimization" element={<OptimizationDetails />} />
-          <Route path="/business-intelligence" element={<IntelligenceDetails />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/cookies-policy" element={<CookiesPolicy />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LayoutPublic />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/security" element={<SecurityDetails />} />
+            <Route path="/maintenance" element={<MaintenanceDetails />} />
+            <Route path="/cloud" element={<CloudDetails />} />
+            <Route path="/web-development" element={<WebDetails />} />
+            <Route path="/business-optimization" element={<OptimizationDetails />} />
+            <Route path="/business-intelligence" element={<IntelligenceDetails />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/cookies-policy" element={<CookiesPolicy />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <ModalCookies />
     </>
   )
